b226d3f38da7175052c7dc2191c9744e522d301e
blackbearlabs888 <tygerearth@gmail.com>
2026-06-23 18:21:54 +0700

    C

--- src/app/api/route.ts
+++
@@ -1,5 +0,0 @@
-import { safeJson } from '@/lib/safe-response'
-
-export async function GET() {
-  return safeJson({ message: "Hello, world!" });
-}
--- src/lib/types.ts
+++
@@ -1,1 +0,0 @@
-export type UserRole = 'OWNER' | 'CREW'
