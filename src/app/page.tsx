2c81e535ee336d1c7bd4cf99850440fd3c9083de
blackbearlabs888 <tygerearth@gmail.com>
2026-07-11 09:38:31 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,7 @@
 'use client'
 
-import { useSearchParams } from 'next/navigation'
-import dynamic from 'next/dynamic'
 import AppShell from '@/components/layout/app-shell'
 
-const TestSuitePage = dynamic(() => import('@/components/pages/test-suite-page'), {
-  ssr: false,
-})
-
 export default function Home() {
-  const searchParams = useSearchParams()
-  const view = searchParams.get('view')
-
-  if (view === 'test-suite') {
-    return <TestSuitePage />
-  }
-
   return <AppShell />
 }
